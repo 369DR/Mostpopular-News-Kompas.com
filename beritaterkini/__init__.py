6081b8f9cd5ea89fa851e9d68a0826dc7448fa66
DR <dapatheai@gmail.com>
2023-03-20 16:54:40 +0700

    Aplikasi membuat modularisasi Berita terkini dari kompas TV

---
+++ beritaterkini/__init__.py
@@ -0,0 +1,16 @@
+import requests
+import bs4
+
+import beritaterkini
+
+
+def ekstrasi_data():
+    pass
+
+def tampilkan_data(result):
+    pass
+
+
+if __name__ == '__main__':
+    result = ekstrasi_data()
+    tampilkan_data(result)
