7d0bbb45384fdecc862965ed9fc8e588e0a933be
DR <dapatheai@gmail.com>
2023-03-23 06:19:49 +0700

    ad description

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ Aplikasi membuat modularisasi Berita terkini dari kompas TV
 import  beritaterpopuler
 
 if __name__ == '__main__':
-    print("Aplikasi utama")
+    print(f"Aplikasi utama menggunakan package yang memiliki deskripsi{beritaterpopuler.description}")
     result = beritaterpopuler.ekstrasi_data()
     beritaterpopuler.tampilkan_data(result)
