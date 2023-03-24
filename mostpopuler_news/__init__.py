1e89886a3578bbe574a5b4b2ffdf6d463a881e7a
DR <dapatheai@gmail.com>
2023-03-24 09:37:34 +0700

    Done

--- mostpopuler_news/__init__.py
+++ mostpopuler_news/__init__.py
@@ -7,35 +7,25 @@ def data_extraction():
     Examples of the 10 most popular news
 
     A.Istri Pamer Harta, Ini Alasan LHKPN Pejabat Kemensetneg Esha Rahmansah Tak Bisa Ditelusuri
-    Dibaca 22.974 kali
 
     B.Isak Tangis Iringi Jenazah Syabda Perkasa Belawa dan Ibunya Saat Tiba di Rumah Duka
-    Dibaca 15.648 kali
 
     C.Viral, Foto Istrinya Pamer Tas Mewah Hermes dan Gucci, Sekda Riau: Itu KW, Belinya di Mangga Dua
-    Dibaca 15.375 kali
 
     D.Kronologi Syabda Perkasa Belawa Meninggal Kecelakaan, Pahlawan Piala Thomas Berpulang...
-    Dibaca 14.137 kali
 
     E.Kabar Duka, Tunggal Putra Indonesia Syabda Perkasa Belawa Meninggal Dunia
-    Dibaca 13.526 kali
 
     F.Luhut ke IMF: Kalian Jangan Macam-macam...
-    Dibaca 13.431 kali
 
     G.Saat "Netizen" Bantu KPK Bongkar Pejabat yang Pamer Harta...
-    Dibaca 8.932 kali
 
     H.Sempat Terbengkalai di Bandara YIA, 38 Calon Jemaah Umrah Asal Rembang Pulang,
     Seorang Perantara Dilaporkan sebagai Penipu
-    Dibaca 8.370 kali
 
     I.Kala Megawati Semprot Ribuan Kades yang Minta Anggaran Jumbo...
-    Dibaca 7.712 kali
 
     J.Kapolda Jateng Resmi Pecat 5 Polisi yang Jadi Calo Penerimaan Bintara Polri 2022
-    Dibaca 7.358 kali
     :return:
     """
     try:
